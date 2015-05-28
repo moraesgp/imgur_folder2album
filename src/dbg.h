5c3ae3b391e400526f34f4acb3b4a77084f8ed29
Gabriel Prado de Moraes <moraesgp@gmail.com>
2015-05-28 17:17:24 -0300

    flushing buffer

--- src/dbg.h
+++ src/dbg.h
@@ -29,16 +29,16 @@ error_code last_error;
 #ifdef NDEBUG
 #define debug(M, ...)
 #else
-#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__); fflush(stderr)
 #endif
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
-#define log_err(M, ...) fprintf(stderr, "[ERROR·] [%s:%d: errno: %s] " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define log_err(M, ...) fprintf(stderr, "[ERROR·] [%s:%d: errno: %s] " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__); fflush(stderr)
 
-#define log_warn(M, ...) fprintf(stderr, "[WARN··] [%s:%d: errno: %s] " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define log_warn(M, ...) fprintf(stderr, "[WARN··] [%s:%d: errno: %s] " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__); fflush(stderr)
 
-#define log_info(M, ...) fprintf(stderr, "[INFO··] [%s:%d] " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_info(M, ...) fprintf(stderr, "[INFO··] [%s:%d] " M "\n", __FILE__, __LINE__, ##__VA_ARGS__); fflush(stderr)
 
 #define throw_error(A, E, M, ...) if(A) { log_err(M, ##__VA_ARGS__); last_error=E; goto error; }
 
